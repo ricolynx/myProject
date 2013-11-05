c407c521f279aa467719877d89cade4e8656c453
Eric <eric.giraud@gmail.com>
2013-11-05 13:56:29 +0100

    chap 1.3

--- MyProject/MyProject.cpp
+++ MyProject/MyProject.cpp
@@ -4,9 +4,22 @@
 #include "stdafx.h"
 #include <iostream>
 
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-	 std::cout << "test";
-	return 0;
+	std::cout << "test" <<std::endl;
+	int a =0 , b=0;
+	std::cin >> a >> b;
+	std::cout << a << " * " << b << " = " << a*b << std::endl;
+
+	std::cout << "the sum of " << a;
+	std::cout << " and " << b;
+	std::cout << " is " << a + b << std::endl;
+
+	std::cout<< "/*";
+	std::cout<< "*/";
+	std::cout<< /* "*/" */";
+	std::cout<< /*"*/"/* "/*"*/;
+	return -1;
 }
 
